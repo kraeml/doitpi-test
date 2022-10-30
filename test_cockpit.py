77545a6cd42882497d916f9cce60198f116e4f4f
kraeml <kraeml@sesamestreet.franken.de>
2022-10-30 17:15:54 +0100

    Test für cockpit hinzugefügt

---
+++ test_cockpit.py
@@ -0,0 +1,8 @@
+import pytest
+
+# ToDo Docker installiert, enabled und running
+def test_services(host):
+    # Services eingerichtet
+    cockpit_service = host.service("cockpit")
+    assert cockpit_service.is_running
+    assert cockpit_service.is_enabled
