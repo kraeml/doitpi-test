8f702fb77a5a656e38e17a834342a370e20edd21
kraeml <kraeml@sesamestreet.franken.de>
2024-03-05 08:13:38 +0100

    Direnv mit System python3 test

--- test_chroot.py
+++ test_chroot.py
@@ -17,6 +17,7 @@ def test_user_file(host):
     (".config/code-server", BASE_USER.pw_dir, BASE_USER.pw_uid),
     ("workspace", BASE_USER.pw_dir, BASE_USER.pw_uid),
     (".borgmatic/", BASE_USER.pw_dir, BASE_USER.pw_uid),
+    (".pyenv/", BASE_USER.pw_dir, BASE_USER.pw_uid),
     (".borgmatic/backup", BASE_USER.pw_dir, BASE_USER.pw_uid),
     (".ansible/roles/deluxebrain.python", BASE_USER.pw_dir, BASE_USER.pw_uid),
     (".ansible/roles/m4rcu5nl.zerotier-one", BASE_USER.pw_dir, BASE_USER.pw_uid),
@@ -31,7 +32,7 @@ def test_user_dir(host, directory, pre_dir, user_uid):
 
 @pytest.mark.parametrize("file, pre_dir, user_uid, contains", [
     ("firstboot.service", "/etc/systemd/system", ROOT_USER.pw_uid, "ExecStart=/firstboot.sh"),
-    (".envrc", BASE_USER.pw_dir + "/.borgmatic", BASE_USER.pw_uid, "layout pyenv 3.12.2"),
+    (".envrc", BASE_USER.pw_dir + "/.borgmatic", BASE_USER.pw_uid, "layout python3"),
     ("firstboot.sh", "", ROOT_USER.pw_uid, "#SOME COMMANDS YOU WANT TO EXECUTE")
 ])
 
